46cf800e3e2b483371fd176df1771c3efb013fb6
AdriLzIf <adrian.lozano.infantes@gmail.com>
2024-03-08 12:53:10 +0100

    M étodo para repartir cartas (nf) v1.1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
-}
+    public static List<String> reparteCartas(List mazo) {
+        Random random = new Random();
+        List<String> mazoJugable = new ArrayList<>();
+
+        for (int a = 0; a < 4; a++) {
+            // Genera números aleatorios dependiendo de la longitud de la lista.
+            int numeroRandom = random.nextInt(mazo.size() / 3 + 1);
+            System.out.println("Número aleatorio: " + numeroRandom);
+
+        }
+    }
